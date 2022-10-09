670fc24f7680066593cbd25c781b6e92199b459c
Dmitrii Builin <d.builin@mail.ru>
2022-10-10 01:08:15 +0300

    les7 added API

--- HW/js/main.js
+++ HW/js/main.js
@@ -61,7 +61,7 @@ const app = new Vue({
             }
             this.countItems = this.cart.length;
         });
-        this.getJson(`${API + this.catalogUrl}`)
+        this.getJson(`API/catalog.json`)
            .then(data => {
                for(let el of data){
                    this.products.push(el);
