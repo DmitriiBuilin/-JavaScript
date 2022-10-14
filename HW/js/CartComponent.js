7a12bb067866c865ce4b636ed9deffde07c334ee
Dmitrii Builin <d.builin@mail.ru>
2022-10-15 00:51:01 +0300

    les8 added cart css-styles

--- HW/js/CartComponent.js
+++ HW/js/CartComponent.js
@@ -14,14 +14,23 @@ Vue.component('cart', {
 Vue.component('cart-item', {
     props: ['product', 'img'],
     template: `
-        <div class="cart-product-item">
-            <img class="cart-image" :src="img" alt="image">
-            <h3 class="">{{product.product_name}}</h3>
-            <p class="">Цена: {{product.price}} $</p>
-            <p class="">Количество: <span>{{product.quantity}}</span></p>
-            <button class="remove-cart-button" :key="product.id_product" @click="$root.removeProduct(product)">Удалить</button>
-            <button class="reduce-cart-button" @click="$root.reduceQty(product)">-</button>
-            <button class="increase-cart-button" @click="$root.increaseQty(product)">+</button>
+        <div class="shopping-cart-item">
+        <img class="cart-image" :src="img" alt="image">
+            <div class="shopping-cart-description">
+                <div class="shopping-cart-header-wrp">
+                    <h2>{{product.product_name}}</h2>
+                    <button type="button" class="shopping-cart-close-button" @click="$root.removeProduct(product)">
+                    </button>
+                </div>
+                <p>Price: {{product.price}} $</p>
+                <div class="shopping-cart-description-quantity">
+                    <p>Quantity:</p>
+                    <input type="number" min="1" :value="product.quantity" placeholder="">
+                </div>
+                <button class="remove-cart-button" :key="product.id_product" @click="$root.removeProduct(product)">Удалить</button>
+                <button class="reduce-cart-button" @click="$root.reduceQty(product)">-</button>
+                <button class="increase-cart-button" @click="$root.increaseQty(product)">+</button>
+            </div>
         </div>
     `
 });
